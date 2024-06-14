afdc2ccf4c231ec9b123697f956f48005d6b9eca
GURDEEP-SINGH01 <gurdeepsingh1431999@gmail.com>
2024-06-15 01:05:24 +0530

    feat(ChatPage): adjusted the width and the styles of the send and received messages

--- client/chat-app/src/component/Layout.jsx
+++ client/chat-app/src/component/Layout.jsx
@@ -3,30 +3,32 @@ import "../index.css";
 import { Chat } from "./chat/Chat";
 import { ConnectedUsers } from "./connectedUsers/ConnectedUsers";
 import { ChatProfile } from "./chatUserProfile/ChatProfile";
+
 export const Layout = ({ userList, loggedUser }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const [openChatProfile, setOpenChatProfile] = useState(false);
+
     if (loggedUser === null) {
         // If loggedUser is not defined, render a loading state or redirect to sign-in
-        return <div className="flex-align-center height-fl" ><div className="loader" /></div>;
+        return <div className="flex-align-center height-fl"><div className="loader" /></div>;
     }
+
     return (
         <div className="width-fl height-fl" style={{ backgroundColor: "pink" }}>
             <div className="flex pad-2-l" style={{ height: "inherit" }}>
-                <div className="col-2 " style={{ width: "20vw", backgroundColor: "pink", border: "1px solid", borderRadius: ".7em 0 0 0" }}>
+                <div className="col-2 user-list" style={{ width: "20vw", backgroundColor: "pink", border: "1px solid", borderRadius: ".7em 0 0 0" }}>
                     <ConnectedUsers
                         userList={userList}
                         setCurrentUser={setCurrentUser}
                         setOpenChatProfile={setOpenChatProfile} />
                 </div>
-                <div className="col-8 " style={{ width: "100vw", backgroundColor: "red" }}>
+                <div className="col-8 chat-container" style={{ width: "100vw", backgroundColor: "red" }}>
                     {loggedUser && <Chat senderId={loggedUser} receiverId={currentUser ? currentUser : loggedUser} />}
                 </div>
-                {openChatProfile && <div className="col-2" style={{ width: "30vw", backgroundColor: "pink", border: "1px solid", }}>
+                {openChatProfile && <div className="col-2 chat-profile" style={{ width: "20vw", backgroundColor: "pink", border: "1px solid" }}>
                     <ChatProfile currentUser={currentUser} setOpenChatProfile={setOpenChatProfile} />
                 </div>}
             </div>
-        </div >
-    )
-}
-
+        </div>
+    );
+};
--- client/chat-app/src/component/chat/Chat.jsx
+++ client/chat-app/src/component/chat/Chat.jsx
@@ -1,22 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import "./chatStyles.css";
 import io from 'socket.io-client';
 import axios from "axios";
 
 export const Chat = ({ senderId, receiverId }) => {
-    const [messages, setMessages] = useState([])
-    const [inputText, setInputText] = useState('')
-    const [typing, setIsTyping] = useState(false);
-    const socket = useRef(null)
-    // console.log(senderId, receiverId)
+    const [messages, setMessages] = useState([]);
+    const [inputText, setInputText] = useState('');
+    const socket = useRef(null);
+
     const sendText = async () => {
         if (inputText.trim() !== '') {
-            await axios.post('http://localhost:9000/chatapp/addMessage',
-                {
-                    to: senderId._id,
-                    from: receiverId._id,
-                    message: inputText
-                })
+            await axios.post('http://localhost:9000/chatapp/addMessage', {
+                to: senderId._id,
+                from: receiverId._id,
+                message: inputText
+            });
             socket.current.emit('sendMessage', {
                 senderId: senderId._id,
                 receiverId: receiverId._id,
@@ -25,57 +23,53 @@ export const Chat = ({ senderId, receiverId }) => {
             setMessages((prevMessages) => [...prevMessages, { fromSelf: true, message: inputText }]);
             setInputText('');
         }
-    }
-    useEffect(() => {
-        socket.current = io('http://localhost:9000');
-    }, [])
-    useEffect(() => {
-        socket.current.emit('addUser', senderId._id);
-    }, [receiverId])
+    };
 
     useEffect(() => {
-        const response = async () => {
-            const dataresponse = await axios.post('http://localhost:9000/chatapp/getMessage', {
-                from: receiverId._id,
-                to: senderId._id
-            })
-            setMessages(dataresponse.data);
-        }
-        response();
+        socket.current = io('http://localhost:9000');
+        socket.current.emit('addUser', senderId._id);
         socket.current.on('getMessage', ({ senderId, message }) => {
             if (senderId === receiverId._id) {
                 setMessages((prevMessages) => [...prevMessages, { fromSelf: false, message }]);
             }
         });
-    }, [senderId, receiverId])
-    const clearAll = () => { setMessages(['']) }
+
+        const fetchMessages = async () => {
+            const response = await axios.post('http://localhost:9000/chatapp/getMessage', {
+                from: receiverId._id,
+                to: senderId._id
+            });
+            setMessages(response.data);
+        };
+
+        fetchMessages();
+
+    }, [senderId, receiverId]);
 
     const onTextChange = (event) => {
-        setInputText(event.target.value)
-    }
-    const lineBreak = (message) => { }
+        setInputText(event.target.value);
+    };
 
     return (
-        <div className='flex-column' >
+        <div className='flex-column chat-wrapper'>
             <header>{receiverId?.username}</header>
-            <section id="display-messages" className='container' style={{ backgroundColor: 'white', overflowY: 'auto' }}>
+            <section id="display-messages" className='container'>
                 {
                     messages.map((message, index) => (
                         <div key={index}
-                            className={`${message?.fromSelf ? "sender" : "recieved"} pad-1-l pad-1-r`}
+                            className={`${message?.fromSelf ? "sender" : "received"} message-box`}
                         >
                             <div className="content">
-                                <p style={{ margin: '0' }}
-                                >{message?.message}</p>
+                                <p style={{ margin: ".4em" }}>{message?.message}</p>
                             </div>
                         </div>
                     ))
                 }
-            </section >
+            </section>
             <div id="write-messages" className='enterText'>
                 <input value={inputText} onChange={onTextChange} placeholder='Enter text here' />
                 <button onClick={sendText}>send</button >
             </div>
-        </div >
-    )
-}
+        </div>
+    );
+};
